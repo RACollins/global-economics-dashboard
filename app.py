039b4602fb1c6cf967adcbb493a05340bca1e770
RACollins <richardcollins128@mail.com>
2024-04-18 23:43:10 +0900

    Added second page

--- app.py
+++ app.py
@@ -30,6 +30,18 @@ st.set_page_config(
 #################
 
 
+@st.cache_data(ttl=3600, show_spinner=False)
+def get_jobs_df(root_dir_path):
+    df = pd.read_csv(root_dir_path + "/data/jobs_vs_gdp_per_capita.csv")
+    return df
+
+
+@st.cache_data(ttl=3600, show_spinner=False)
+def get_forex_df(root_dir_path):
+    df = pd.read_csv(root_dir_path + "/data/forex_vs_gdp_per_capita.csv")
+    return df
+
+
 def apply_graph_stylings(fig):
     fig.update_traces(textposition="top center")
     fig.update_layout(plot_bgcolor="white")
@@ -61,28 +73,27 @@ def main():
 
     ### Side bar
     with st.sidebar:
-        tab_headers = {
-            "tab1": "Salary vs. GDP per capita",
-            "tab2": "Forex vs. GDP per capita",
-        }
-        st.header("Info...")
-        with st.expander(tab_headers["tab1"]):
-            st.write("Text1 " "Text2")
-        with st.expander(tab_headers["tab2"]):
-            st.write("Text1 " "Text2")
+        st.header("Universal options")
+        with st.container(border=True):
+            log_x = st.checkbox("log_x")
+            log_y = st.checkbox("log_y")
+        with st.container(border=True):
+            show_pop = st.checkbox("Show Population")
 
     ### Tabs
+    tab_headers = {
+        "tab1": "Salary vs. GDP per capita",
+        "tab2": "Forex vs. GDP per capita",
+    }
     tab1, tab2 = st.tabs([tab_headers[k] for k, v in tab_headers.items()])
 
     with tab1:
         ### Get dataframe
-        df = pd.read_csv(root_dir_path + "/data/jobs_vs_gdp_per_capita.csv")
+        jobs_df = get_jobs_df(root_dir_path)
 
         ### Filters
-        left_buffer_cols, left_col, right_col, right_buffer_cols = st.columns(
-            [2, 4, 4, 2]
-        )
-        with left_col:
+        left_jobs_buffer, centre_jobs_col, right_jobs_buffer = st.columns([2, 8, 2])
+        with centre_jobs_col:
             with st.container(border=True):
                 selected_year = st.selectbox(
                     label="Year", options=list(range(1980, 2030)), index=44
@@ -92,16 +103,13 @@ def main():
                     options=["Bricklayer", "Doctor", "Nurse", "All"],
                     index=2,
                 )
-        with right_col:
-            with st.container(border=True):
-                log_x = st.checkbox("log_x")
-                log_y = st.checkbox("log_y")
-            with st.container(border=True):
-                show_pop = st.checkbox("Show Population")
 
         ### Apply filters
         job_df = (
-            df.loc[(df["Job"] == selected_job) & (df["Year"] == selected_year), :]
+            jobs_df.loc[
+                (jobs_df["Job"] == selected_job) & (jobs_df["Year"] == selected_year),
+                :,
+            ]
             .drop(columns=["Unnamed: 0"])
             .reset_index(drop=True)
         )
@@ -136,7 +144,36 @@ def main():
         with st.container(border=True):
             st.plotly_chart(fig, theme=None, use_container_width=True)
     with tab2:
-        st.info("Something should go here...")
+        ### Get dataframe
+        forex_df = get_forex_df(root_dir_path)
+        # st.dataframe(forex_df)
+
+        ### Plot
+        size = "Population" if show_pop else None
+        fig = px.scatter(
+            forex_df,
+            x="GDP_per_capita_USD",
+            y="Forex_Reserves_per_person_USD",
+            color="Region",
+            color_discrete_sequence=["red", "magenta", "goldenrod", "green", "blue"],
+            category_orders={
+                "Region": ["Asia", "Americas", "Africa", "Europe", "Oceania"]
+            },
+            size=size,
+            size_max=80,
+            hover_data={"Country": True, "Population": True},
+            # text="Country",
+            trendline="ols",
+            trendline_scope="overall",
+            trendline_options=dict(log_x=log_x, log_y=log_y),
+            trendline_color_override="black",
+            title="Forex Reserves Including Gold per Person VS. GDP per capita (2024)",
+            log_x=log_x,
+            log_y=log_y,
+        )
+        fig = apply_graph_stylings(fig)
+        with st.container(border=True):
+            st.plotly_chart(fig, theme=None, use_container_width=True)
 
 
 if __name__ == "__main__":
